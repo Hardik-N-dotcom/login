6cc7f69cd8848edc5fc04196e65539ca22ae36b7
Hardik-N-dotcom <hrdknarayan@gmail.com>
2025-06-29 23:53:19 +0530

    Update index.js

--- Quicklogin/server/index.js
+++ Quicklogin/server/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: '*',
+  origin: 'https://login-blush-two.vercel.app/',
   credentials: true
 }));
 
